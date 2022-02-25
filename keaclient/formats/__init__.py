05435502692557b5db17d1c1b698bac7a22652f1
Fergus Noble <fergusnoble@gmail.com>
2022-02-25 14:40:34 -0800

    Make formats configurable

--- keaclient/formats/__init__.py
+++ keaclient/formats/__init__.py
@@ -3,7 +3,7 @@ import confuse
 
 logger = logging.getLogger("KeaClient")
 
-def sbp_format_from_config(driver):
+def sbp_format_from_config(config, driver):
   from sbp.client import Handler, Framer
   return Handler(Framer(driver.read, None, verbose=True))
 
@@ -12,5 +12,13 @@ FORMATS = {
 }
 
 def format_from_config(config, driver):
-  format = config['format'].as_choice(FORMATS.keys())
-  return FORMATS[format](driver)
+  formats_cfg = config['format'].get()
+  if len(formats_cfg) != 1:
+    raise confuse.exceptions.ConfigValueError('Exactly one format should be specified')
+
+  format_type = list(formats_cfg.keys())[0]
+  if format_type not in FORMATS:
+    raise confuse.exceptions.ConfigValueError(f'Unknown format type "{format_type}"')
+
+  format_config = config['format'][format_type]
+  return FORMATS[format_type](format_config, driver)
