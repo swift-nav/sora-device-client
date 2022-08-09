4907e614e3de631000e61dac5608c6484b09b56f
Narthana Epa <narthana.epa@gmail.com>
2022-08-09 11:59:14 +1000

    Fix config key for device-id

--- sora_device_client/cli/login/__init__.py
+++ sora_device_client/cli/login/__init__.py
@@ -1,6 +1,13 @@
 import click
+import uuid
 
 
 @click.command()
-def login():
-    pass
+@click.pass_obj
+@click.option("-i", "--device-id", help="Device Id")
+def login(config, device_id):
+    if device_id:
+        device_uuid = uuid.UUID(device_id)
+        config["device-id"].set(str(device_uuid))
+
+    click.echo(config.dump())
