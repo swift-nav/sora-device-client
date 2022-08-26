d4bf93c32b7f62ec8508c1a6bc5d46ff6bffe771
Narthana Epa <narthana.epa@gmail.com>
2022-08-26 14:44:30 +1000

    Read auth0 info from server in config

--- sora_device_client/cli/login.py
+++ sora_device_client/cli/login.py
@@ -1,14 +1,16 @@
 import tomlkit
-import typer
 
 from deepmerge import always_merger
 from rich import print
-from typing import Optional
 from uuid import UUID
 
-from ..auth0 import Auth0Client, AUTH0_AUDIENCE, AUTH0_CLIENT_ID, AUTH0_DOMAIN
-from ..config import read_data, write_data
+from ..auth0 import Auth0Client
+from ..auth0.info import auth0_auth_server_info
+from ..client import device_service_channel
+from ..config import read_config, read_data, write_data
 from ..exceptions import DataFileNotFound
+
+import sora.device.v1beta.service_pb2_grpc as device_grpc
 
 
 def login():
@@ -24,14 +26,21 @@ def login():
 
     try:
         device = data["device"]
-        device_uuid = UUID(device["id"])
+        device_uuid = UUID(str(device["id"]))
         print(f"Already logged in as device {device_uuid}.")
         return
     except tomlkit.exceptions.NonExistentKey:
-        client = Auth0Client(AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_AUDIENCE)
-        device_uuid, device_access_token = client.register_device()
-        always_merger.merge(data, {"device": {"access_token": device_access_token}})
+        config = read_config()
+        server = config["server"]
+        with device_service_channel(
+            server["host"], server["port"], server["disable_tls"]
+        ) as channel:
+            stub = device_grpc.DeviceServiceStub(channel)
+            info = auth0_auth_server_info(stub)
+            client = Auth0Client(info=info)
 
+    device_uuid, device_access_token = client.register_device()
+    always_merger.merge(data, {"device": {"access_token": device_access_token}})
     always_merger.merge(data, {"device": {"id": str(device_uuid)}})
 
     write_data(data)
