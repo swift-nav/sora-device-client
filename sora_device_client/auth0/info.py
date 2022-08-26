f1d86229d3cf35ca4187f1f5543e89a02f39f67d
Narthana Epa <narthana.epa@gmail.com>
2022-08-26 14:35:02 +1000

    Wrap the AuthServerInfo repo

--- sora_device_client/auth0/info.py
+++ sora_device_client/auth0/info.py
@@ -1,13 +1,11 @@
-import grpc
 import logging
 
 from dataclasses import dataclass
-from google.protobuf.struct_pb2 import Struct
-from google.protobuf.timestamp_pb2 import Timestamp
 
-import sora.v1beta.common_pb2 as common_pb
-import sora.device.v1beta.service_pb2_grpc as device_grpc
-import sora.device.v1beta.service_pb2 as device_pb2
+from rich import print
+
+from sora.device.v1beta.service_pb2 import AuthServerInfoRequest
+from sora.device.v1beta.service_pb2_grpc import DeviceServiceStub
 
 log = logging.getLogger(__name__)
 
@@ -19,21 +17,9 @@ class Auth0AuthServerInfo:
     audience: str
 
 
-def auth0_auth_server_info(channel) -> Auth0AuthServerInfo:
-    try:
-        resp: device_pb2.AuthServerInfoResponse = channel.AuthServerInfo(
-            device_pb2.AuthServerInfoRequest()
-        )
-        info = resp.info
-        auth0 = info.auth0
-        auth_server_url = auth0.auth_server_url
-        client_id = auth0.client_id
-        audience = auth0.audience
-    except:
-        raise
-
-    channel.close()
-
+def auth0_auth_server_info(stub: DeviceServiceStub) -> Auth0AuthServerInfo:
+    resp = stub.AuthServerInfo(AuthServerInfoRequest())
+    auth0 = resp.auth0
     return Auth0AuthServerInfo(
-        auth_server_url=auth_server_url, client_id=client_id, audience=audience
+        host=auth0.host, client_id=auth0.client_id, audience=auth0.audience
     )
